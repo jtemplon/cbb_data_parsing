cc56cb42fd960c02a76e2537756fb78adc7dfaa1
John Templon <jtemplon@gmail.com>
2014-02-17 19:51:52 -0500

    Added a way to get game IDs for any team plus updated

---
+++ get_game_links_for_team.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup as bs
+import urllib2
+
+def get_game_links(team_page):
+    f = urllib2.urlopen(team_page)
+    html = f.read()
+    text = bs(html)
+    tables = text.find_all("table", { "class": "mytable" })
+    game_table = tables[0]
+    gt_rows = game_table.find_all("tr")
+    game_links = []
+    for r in gt_rows[2:]:
+        cells = r.find_all("td")
+        #print cells
+        if cells[2].a != None:
+            l = cells[2].a["href"]
+            lid = l.split("?")[0].replace("/game/index/", "")
+            o = cells[1].a.string
+            link_tuple = (lid, o)
+            game_links.append(link_tuple)
+    return game_links
+
+game_links = get_game_links("http://stats.ncaa.org/team/index/11540?org_id=617")
+for gl in game_links:
+    print gl[0], gl[1]
